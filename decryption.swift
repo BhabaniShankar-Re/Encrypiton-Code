e436438a2fb348517ca637733cf0906e7001f642
BsB-ave <52691015+BsB-ave@users.noreply.github.com>
2020-02-05 00:25:25 +0530

    Create decryption.swift

---
+++ decryption.swift
@@ -0,0 +1,13 @@
+//Decryption
+let oldDecArray = Array(newArray)
+var decArray = ""
+for value in  stride(from: 1, to: newArray.count, by: 2){
+    var temp = ""
+    let toMove = Int(String(oldDecArray[value]))!
+    for _ in 1...toMove{
+        temp.append(oldDecArray[value - 1])
+    }
+    decArray.append(contentsOf: temp)
+}
+
+print(decArray)
